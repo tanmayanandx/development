e09f80d69044e322ce8fa5feb0875ac44cb67a67
tanmayanandx <54024121+tanmayanandx@users.noreply.github.com>
2021-06-24 16:12:07 +0530

    Add files via upload

---
+++ WebDev/JavaScript/Intro To DOM/Select/app.js
@@ -0,0 +1,18 @@
+const allLinks = document.querySelectorAll('a');
+
+// for (let link of allLinks) {
+//     link.innerText = 'I AM A LINK!!!!'
+// }
+
+
+for (let link of allLinks) {
+    link.style.color = 'rgb(0, 108, 134)';
+    link.style.textDecorationColor = 'magenta';
+    link.style.textDecorationStyle = 'wavy'
+}
+
+const allImages = document.getElementsByTagName('IMG');
+
+for (let IMG of allImages) {
+    console.log(IMG.src);
+}
