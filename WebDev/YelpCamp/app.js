99fbb7c5aa5af764e6b751ca2eba6b7f3515557e
tanmayanandx <54024121+tanmayanandx@users.noreply.github.com>
2021-11-02 16:58:55 +0530

    Add files via upload

--- WebDev/YelpCamp/app.js
+++ WebDev/YelpCamp/app.js
@@ -1,21 +1,25 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
-const Campground = require('./models/campground');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
+const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 	useNewUrlParser: true,
+	useCreateIndex: true,
 	useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-	console.log('Database Connected');
+	console.log('Database connected');
 });
 
+const app = express();
+
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -24,14 +28,11 @@ app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
 	res.render('home');
-	res.send('Hello frm YelpCamp!');
 });
-
 app.get('/campgrounds', async (req, res) => {
 	const campgrounds = await Campground.find({});
 	res.render('campgrounds/index', { campgrounds });
 });
-
 app.get('/campgrounds/new', (req, res) => {
 	res.render('campgrounds/new');
 });
@@ -63,12 +64,6 @@ app.delete('/campgrounds/:id', async (req, res) => {
 	await Campground.findByIdAndDelete(id);
 	res.redirect('/campgrounds');
 });
-
-// app.get('/makecampground', async (req, res) => {
-// 	const camp = new Campground({ title: 'My Backyard', description: 'Cheap Camping!!' });
-// 	await camp.save();
-// 	res.send(camp);
-// });
 
 app.listen(3000, () => {
 	console.log('Serving on port 3000');
